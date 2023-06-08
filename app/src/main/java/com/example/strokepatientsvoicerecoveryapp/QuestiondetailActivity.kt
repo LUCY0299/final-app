84c653c6271ada36e3a8e21b780c527f45c96a39
JennyChang1124 <athome889@gmail.com>
2023-06-08 11:43:13 +0800

    master to main 0608

---
+++ app/src/main/java/com/example/strokepatientsvoicerecoveryapp/QuestiondetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.strokepatientsvoicerecoveryapp
+
+
+import android.widget.Button
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class QuestiondetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.question_detail)
+    }
+}

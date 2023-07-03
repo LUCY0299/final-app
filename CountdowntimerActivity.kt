0ffbb006cdbdc6617cfdcf5e972dde6abcb19e84
張茜雯 <112846217+JennyChang1124@users.noreply.github.com>
2023-07-03 17:49:53 +0800

    Add files via upload

---
+++ CountdowntimerActivity.kt
@@ -0,0 +1,46 @@
+package com.example.strokepatientsvoicerecoveryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.os.CountDownTimer
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class CountdowntimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.countdown_timer)
+
+        val textView = findViewById<TextView>(R.id.countdown_timer)
+
+        val timeValue = intent.getIntExtra("timeValue", 0) // 從Intent中檢索時間值
+        val timeDuration = TimeUnit.MINUTES.toMillis(timeValue.toLong()) // 設定倒數時間
+        val tickInterval: Long = 10 //接收回調的間隔
+
+
+        object : CountDownTimer(timeDuration, tickInterval) {
+            var millis: Long = 1000 //1000=1秒
+            override fun onTick(millisUntilFinished: Long) {
+                millis = millis - tickInterval
+                if (millis == 0L) millis = 1000
+                val timerText = String.format(
+                    Locale.getDefault(), "%2d:%02d",
+                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                            TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(
+                                    millisUntilFinished
+                                )//公式
+                            ),
+                    millis
+                )
+                textView.text = timerText
+            }
+
+            override fun onFinish() {
+                textView.text = "時間到了"
+            }//倒數完畢時
+        }.start()
+    }
+}
